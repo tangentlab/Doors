fff4f0f350ef0f252411393a68f0b745ca279ef6
tangentlab <guidewire@gmail.com>
2026-02-16 00:51:45 -0600

    Update af-comps.js

--- js/af-comps.js
+++ js/af-comps.js
@@ -1,3 +1,35 @@
+AFRAME.registerComponent("infoicon", {
+  schema: {
+    linkto: { type: "string", default: "" },
+    infogroup: { type: "string", default: "" },
+  },
+  init() {
+    this.el.setAttribute("src", "#info-icon");
+    this.el.setAttribute("scale", "0.5 0.5 0.5");
+    this.el.setAttribute("look-at", "#cam");
+    var data = this.data;
+
+    this.el.addEventListener("click", function () {
+      this.setAttribute("scale", "0.7 0.7 0.7");
+      this.setAttribute("src", data.infolink);
+    });
+  },
+});
+
+AFRAME.registerComponent("hotspots", {
+  init: function () {
+    this.el.addEventListener("reloadspots", function (evt) {
+      var currspotgroup = document.getElementById(evt.detail.currspots);
+      currspotgroup.setAttribute("scale", "0 0 0");
+
+      var newspotgroup = document.getElementById(evt.detail.newspots);
+      newspotgroup.setAttribute("scale", "1 1 1");
+    });
+  },
+  update: function () {},
+  tick: function () {},
+});
+
 AFRAME.registerComponent("hotspot", {
   schema: {
     color: { default: "#FFF" },
